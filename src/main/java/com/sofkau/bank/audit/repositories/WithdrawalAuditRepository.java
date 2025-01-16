498409b6b11bcfb99936bbaa50ff9af23787e8c3
Andrés Téllez <davidroa8@hotmail.com>
2025-01-16 00:52:11 -0500

    feat: add withdrawal audit service

--- src/main/java/com/sofkau/bank/audit/repositories/WithdrawalAuditRepository.java
+++ src/main/java/com/sofkau/bank/audit/repositories/WithdrawalAuditRepository.java
@@ -2,8 +2,10 @@ package com.sofkau.bank.audit.repositories;
 
 import com.sofkau.bank.audit.documents.WithdrawalAudit;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
 public interface WithdrawalAuditRepository extends ReactiveMongoRepository<WithdrawalAudit, UUID> {
+    Flux<WithdrawalAudit> findBySourceAccount(UUID sourceAccount);
 }
